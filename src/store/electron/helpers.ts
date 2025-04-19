15f74a7ef1cfe28948e67018a3bdbf00af8a794d
arvinxx <arvinx@foxmail.com>
2025-04-19 14:17:17 +0800

    clear

--- src/store/electron/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { useGlobalStore } from '@/store/global/index';
-import { globalGeneralSelectors } from '@/store/global/selectors';
-
-const getCurrentLanguage = () => globalGeneralSelectors.currentLanguage(useGlobalStore.getState());
-
-export const globalHelpers = { getCurrentLanguage };
